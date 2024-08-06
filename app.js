f7d7b0adae5935934cd1ee53fca0ca9c8b41c9d7
Belen Humbert <belenhumbert69@gmail.com>
2024-08-06 03:48:38 -0300

    se agregan nuevas certificaciones, se modifica cv

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//Función para filtrar las categorías de los trabajos
 function verCategoria(cat){
     const items = document.getElementsByClassName("item");
     for(let i=0; i < items.length;i++){
